b094b60751b158102594134b26e4ed404a6f0fc2
David Liman <limanoit@gmail.com>
2017-04-11 15:00:08 -0700

    deps is not starting up

--- src/dv.erl
+++ src/dv.erl
@@ -1,17 +1,12 @@
 -module(dv).
 -behavior(application).
 
--export([start/0, start/2,
-         stop/0, stop/1]).
-
-start() ->
-    application:ensure_started(dv_app).
+-export([start/2, stop/1]).
 
 start(_Type, _Args) ->
-    ok.
 
-stop() ->
-    ok.
+    sup:start_link().
+
 
 stop(_State) ->
     ok.
