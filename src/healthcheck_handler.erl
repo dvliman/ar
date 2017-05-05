5c1782bcdf99750fe42df01b7d7bd219e0bdc2f8
David Liman <limanoit@gmail.com>
2017-05-05 11:43:08 -0700

    add healthcheck

--- src/healthcheck_handler.erl
+++ src/healthcheck_handler.erl
@@ -1,1 +1,12 @@
 -module(healthcheck_handler).
+
+-export([init/2]).
+
+init(Req, Opts) ->
+    NewReq = cowboy_req:reply(200,
+        #{<<"Content-Type">> => <<"application/json">>},
+        jiffy:encode(#{status => ok, time => utils:now()}),
+        Req),
+    
+    {ok, NewReq, Opts}.
+
